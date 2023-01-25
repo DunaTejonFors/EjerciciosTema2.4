028ad33637564017eb7d02633a6d66913e644a88
DunaTejonFors <duna1999@gmail.com>
2023-01-25 15:59:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+lista_num = []
+for i in range(1, 101):
+    lista_num.append(i)
+
+lista_num_inv = sorted(lista_num, reverse=True)
+
+print(lista_num_inv)
+
+#for i in range(0,100):
+    #print(lista_num_inv[i])
